b38c17048ea9ddb99fdf574cbc39e032a5b006c5
eren4321 <43602937+eren4321@users.noreply.github.com>
2020-03-30 16:17:04 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,3 @@
-from flask import Flask, render_template, jsonify
-import mysql.connector
+from flask import Flask
 
 app = Flask(__name__)
-
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="123123",
-    database="mydata",
-
-
-)
-print(mydb)
-
-sql_select_Query = "select * from data"
-mycursor = mydb.cursor()
-mycursor.execute(sql_select_Query)
-records = mycursor.fetchall()
-print("Total number of rows in Laptop is: ", mycursor.rowcount)
-
-@app.route('/')
-def index():
-    return render_template('chart.html')
-
-
-@app.route('/data')
-def data():
-    dolar = []
-    for row in records:
-        if row[1] == "o10":
-            dolar.append(row[5])
-    return jsonify({'result' : dolar})
-
-if __name__ == '__main__':
-    app.run(debug=True)
